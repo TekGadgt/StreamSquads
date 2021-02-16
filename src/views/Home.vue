8abd40e42bb3fe3dfa13ae8cc7a8aff2604ec537
Ryan McGovern <tekgadgt@devportfol.io>
2021-02-16 15:59:24 -0500

    Initial Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>{{ nodeVersion }}</p>
   </div>
 </template>
 
@@ -13,6 +14,11 @@ export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      nodeVersion: process.version
+    };
   }
 };
 </script>
