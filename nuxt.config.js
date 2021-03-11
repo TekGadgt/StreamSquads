77437aca376bb9f50484224eda014e63f648587e
Ryan McGovern <tekgadgt@devportfol.io>
2021-03-11 17:53:07 -0500

    chore: added additional dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'streamsquads',
+    title: 'StreamSquads',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -29,6 +29,13 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    '@nuxtjs/composition-api',
+    '@nuxtjs/svg',
+    '@nuxtjs/fontawesome',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/date-fns',
+    '@nuxt/image',
+    '@nuxtjs/mdx',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -37,7 +44,37 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: process.env.FireBaseAPIKey,
+          authDomain: process.env.FireBaseAuthDomain,
+          projectId: process.env.FireBaseProjectID,
+          storageBucket: process.env.FireBaseStorageBucket,
+          messagingSenderId: process.env.FireBaseMessageingSenderID,
+          appId: process.env.FireBaseAppID,
+          measurementId: process.env.FireBaseMeasurementID,
+        },
+        services: {
+          auth: false, // Just as example. Can be any other service.
+        },
+      },
+    ],
+    [
+      'nuxt-stripe-module',
+      {
+        publishableKey: process.env.StripePublishableKey,
+      },
+    ],
+    'nuxt-feature-toggle',
   ],
+
+  publicRuntimeConfig: {
+    featureToggle: {
+      toggles: {},
+    },
+  },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
